e857f1455fd6491889ed65b2583eec491fd64492
Tommy Werner <wtwerner@gmail.com>
2020-05-16 19:05:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,6 +21,7 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	programmer_hash[:grace_hopper][:known_for]
 	
 	programmer_hash = 
  		{
