ef0ab301fb8c2be10e11908c175cbf23988076c5
Tommy Werner <wtwerner@gmail.com>
2020-05-16 19:11:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]
+     programmer_hash_string = programmer_hash[:dennis_ritchie][:languages]
+     programmer_hash_string
 end
 
 def adding_matz
