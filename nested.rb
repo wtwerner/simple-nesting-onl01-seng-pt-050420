a89f32882fc2b6b8e7f29cff552a113a1c6f6adf
Tommy Werner <wtwerner@gmail.com>
2020-05-16 19:22:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] = :known_for, :languages
-programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-programmer_hash{:yukihiro_matsumoto][:languages] = "LISP"
+programmer_hash[:yukihiro_matsumoto] = 
     
 programmer_hash
 end
